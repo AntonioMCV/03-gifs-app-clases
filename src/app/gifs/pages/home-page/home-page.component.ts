52f35c93cd11ef4c2764001ec65a2c9f5039f852
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2024-03-25 16:37:23 +0100

    eliminacion de archivos innecesarios

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css',
+  styles: '',
 })
 export class HomePageComponent {}
