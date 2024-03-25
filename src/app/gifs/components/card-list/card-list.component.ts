7ff43926c26c9c7b40b27ec20784aa9b5bee44a7
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2024-03-25 14:25:41 +0100

    eliminacion de archivos innecesarios

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
-  styleUrl: './card-list.component.css',
+  styles: '',
 })
 export class CardListComponent {}
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -4,7 +4,12 @@ import { GifsService } from '../../../gifs/services/gifs.service';
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css',
+  styles: `
+    #sidebar {
+      height: 100vh;
+      width: 250px;
+    }
+  `,
 })
 export class SidebarComponent {
   constructor(private gifsService: GifsService) {}
